4453c0a21befc855395e4916bf1efe492dec976b
Rashmi Bachani <echorashmi@gmail.com>
2020-07-27 18:58:51 -0400

    Defanging Problem

---
+++ defanging-an-ip-address.c
@@ -0,0 +1,44 @@
+/*
+    C Program to Defang an IP Address. 
+    Example
+    Input:  1.1.1.1
+    Output: 1[.]1[.]1[.]1
+    
+    Pending Submit, leetcode platform is currently having issues
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * defangIPaddr(char * address);
+
+int main(void)
+{
+    char *address = "255.100.50.0";
+    printf("%s\n", defangIPaddr(address));
+}
+
+char * defangIPaddr(char * address){
+    char *defangedAddress;
+    defangedAddress = malloc(sizeof(address) + 6);
+    int j = 0;
+    
+    int length = strlen(address);
+    for (int i = 0; i < length; i++)
+    {
+        if(address[i] == '.')
+        {
+            defangedAddress[j] = '[';
+            defangedAddress[j+1] = '.';
+            defangedAddress[j+2] = ']';
+            j = j+3;
+        }
+        else
+        {
+            defangedAddress[j] = address[i];
+            j++;
+        }
+    }
+    return defangedAddress;
+}
