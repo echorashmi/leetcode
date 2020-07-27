259e39d4424eaa249695623e54c1604f00f1beb9
Rashmi Bachani <echorashmi@gmail.com>
2020-07-27 19:04:53 -0400

    Number of Steps to reduce a number

---
+++ number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,37 @@
+/* 
+    C Program to count the number of steps to reduce a number to Zero.
+    
+    Spec: Given a non-negative number num, return the number of steps to reduce it to zero. 
+    If current number is even, divide by 2, otherwise subtract 1 from it. 
+    
+    Example Input: 14
+    Output: 6
+    
+    Pending Submission, Leetcode is down. 
+*/
+
+#include <stdio.h>
+
+int numberOfSteps (int num);
+
+int main(void)
+{
+    printf("%i", numberOfSteps(123));
+}
+
+int numberOfSteps (int num){
+    int count;
+    while (num > 0)
+    {
+        if (num % 2 == 0)
+        {
+            num = num / 2;
+        }
+        else
+        {
+            num = num - 1;
+        }
+        count++;
+    }
+    return count;
+}
